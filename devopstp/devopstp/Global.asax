0e6fd6ebd8f1e5df64d8e83b929162449e3585b9
Anass Abouzakaria <abouzakaria.anass@gmail.com>
2020-04-07 04:08:36 +0000

    First commit

---
+++ devopstp/devopstp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="devopstp.Global_asax" Language="vb" %>
---
+++ devopstp/devopstp/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+<Assembly: AssemblyTitle("devopstp")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("devopstp")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("60074b72-323c-4b48-aa3d-75b0da4ea8c6")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
